5a1dc5d0ef3e67a4ed2a57762d145127c8889734
mckasap <mckasap@gmail.com>
2019-03-22 14:54:05 +0300

    Download image

--- app/src/main/java/org/kasapbasi/week20190701/MainActivity.java
+++ app/src/main/java/org/kasapbasi/week20190701/MainActivity.java
@@ -1,13 +1,130 @@
 package org.kasapbasi.week20190701;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView iv;
+
+    public class DownloadImageTask extends AsyncTask<String,Integer, Bitmap>{
+
+
+        /**
+         * Override this method to perform a computation on a background thread. The
+         * specified parameters are the parameters passed to {@link #execute}
+         * by the caller of this task.
+         * <p>
+         * This method can call {@link #publishProgress} to publish updates
+         * on the UI thread.
+         *
+         * @param strings The parameters of the task.
+         * @return A result, defined by the subclass of this task.
+         * @see #onPreExecute()
+         * @see #onPostExecute
+         * @see #publishProgress
+         */
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+
+            try {
+                URL url= new URL(strings[0]);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.connect();
+                InputStream in= con.getInputStream();
+                final Bitmap bmp = BitmapFactory.decodeStream(in);
+                publishProgress(10);
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        iv.setImageBitmap(bmp);
+                    }
+                });
+
+                return bmp;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+        /**
+         * Runs on the UI thread after {@link #publishProgress} is invoked.
+         * The specified values are the values passed to {@link #publishProgress}.
+         *
+         * @param values The values indicating progress.
+         * @see #publishProgress
+         * @see #doInBackground
+         */
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+
+        }
+
+        /**
+         * <p>Runs on the UI thread after {@link #doInBackground}. The
+         * specified result is the value returned by {@link #doInBackground}.</p>
+         *
+         * <p>This method won't be invoked if the task was cancelled.</p>
+         *
+         * @param bitmap The result of the operation computed by {@link #doInBackground}.
+         * @see #onPreExecute
+         * @see #doInBackground
+         * @see #onCancelled(Object)
+         */
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+           // iv.setImageBitmap(bitmap);
+
+        }
+    }
+
+
+    public void fetchImage(View v){
+
+  DownloadImageTask dit = new DownloadImageTask();
+  dit.execute("https://vignette.wikia.nocookie.net/world-of-light-and-dark/images/c/c8/QueenAurora.jpg");
+
+//        try {
+////            Bitmap bmp= dit.execute("https://vignette.wikia.nocookie.net/" +
+////                     "world-of-light-and-dark/images/c/c8/QueenAurora.jpg").get();
+////            iv.setImageBitmap(bmp);
+//        }
+////        catch (ExecutionException e) {
+////            e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv=(ImageView) findViewById(R.id.imageView);
+
+
     }
 }
